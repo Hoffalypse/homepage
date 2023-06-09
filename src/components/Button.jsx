9c905e122142a838e4b8d7cfc1d2449b26bb7dc6
Bryan W <bhoff1980@gmail.com>
2023-06-08 22:45:34 -0600

    altered vite config for hot reload

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 const Button = ({ styles }) => (
-  <Link to='./contact'><button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
+  <Link to='/contact'><button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
     Get Started
   </button>
   </Link>
